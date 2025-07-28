3933c33637030de1aea4b286819416782159b79b
ajastou <ajastou@gmail.com>
2025-07-28 22:45:03 +0000

    Update finInclusion.py
    Signed-off-by: ajastou <ajastou@gmail.com>

--- finInclusion.py
+++ finInclusion.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 import joblib
-streamlit run finInclusion.py
 # Titre
 st.title("Prédiction Inclusion financiere en Afrique (Banque)")
 st.header("Classification Machine learning")
